24788ec9596fb6ad44d40abb4402b2394b5656d6
Zexi Li <zexi.li@icloud.com>
2021-10-26 09:55:52 +0800

    centos7: dracut add nvme driver

--- scripts/centos/dracut-config.sh
+++ scripts/centos/dracut-config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo 'add_drivers+="hpsa mptsas mpt2sas mpt3sas megaraid_sas mptspi virtio virtio_ring virtio_pci virtio_scsi virtio_blk vmw_pvscsi smartpqi aacraid"' >> /etc/dracut.conf
+echo 'add_drivers+="hpsa mptsas mpt2sas mpt3sas megaraid_sas mptspi virtio virtio_ring virtio_pci virtio_scsi virtio_blk vmw_pvscsi smartpqi aacraid nvme nvme_core"' >> /etc/dracut.conf
 dracut -f
